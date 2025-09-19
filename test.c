cd6d20cc381311426c94b487c8d444bad6c0f150
alice <3608034157@qq.com>
2025-09-19 10:09:48 +0800

    alice:add greet()

--- test.c
+++ test.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+
+void greet()
+{
+	printf("hello\n");
+}
+
+
 int main()
 {
     printf("hey, it's bob!\n");
 
     printf("hi from alice!\n");
 
-
+    greet();
     return 0;
 }
